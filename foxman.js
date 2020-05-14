ae10b2aded575418e60b159293ee8961ffa32384
claes-magnus <claes-magnus@herebeseaswines.net>
2020-05-14 22:13:10 +0200

    Add keyboard event handling

--- foxman.js
+++ foxman.js
@@ -1,6 +1,6 @@
-const readline = require('readline');
 const rawLevel = require('./level');
 const level = require('./init-level')(rawLevel);
+const readline = require('readline');
 const printState = require('./print-state');
 const initCreatures = require('./init-creatures');
 require('cli-cursor').hide();
@@ -9,4 +9,29 @@ const initialState = {
   level,
   creatures: initCreatures(level)
 };
+
+const defaultEventState = () => ({
+  down: false,
+  up: false,
+  right: false,
+  left: false
+});
+let events = { ...defaultEventState() };
+const commands = {
+  q: () => process.exit(0),
+  up: () => events.down = true,
+  right: () => events.right = true,
+  down: () => events.down = true,
+  left: () => events.left = true
+};
+const handleKeyEvent = (_, key) => key.name in commands && commands[key.name](); 
+process.stdin.setRawMode(true);
+readline.emitKeypressEvents(process.stdin);
+process.stdin.on('keypress', handleKeyEvent);
+
+setInterval(() => {
+  console.log(events)
+  events = { ...defaultEventState() };
+}    
+, 1000 / 30)
 

16be33182c19ffc8e1b433051444e2ba1b6fb0bc
claes-magnus <claes-magnus@herebeseaswines.net>
2020-05-14 21:07:44 +0200

    Add graphics

---
+++ graphics.js
@@ -0,0 +1,10 @@
+const chalk = require('chalk');
+
+const graphics = {
+  x:     chalk.blue('██'),
+  m:     '👹',
+  f:     '🦊',
+  ' ':   chalk.yellow('..'),
+  score: '💎',
+};
+module.exports = graphics;
